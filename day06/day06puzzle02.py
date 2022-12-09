040433a69c9863eeb3ca3ef38d566938661a836a
Jekky24 <jacky.hanh@live.com.au>
2022-12-09 23:01:00 +1100

    Day 6 Solutions

---
+++ day06/day06puzzle02.py
@@ -0,0 +1,34 @@
+# Import the .txt input file and save it as a file
+with open("input_day06.txt") as f:
+    # Save each line of the .txt file as a string including new line character \n
+    lines = f.readlines()
+
+# When lines is imported and read, it saves each line as a separate element of
+# a list. Since there is only one line in the input file, converting the list to
+# a string makes easier handling
+lines = lines[0]
+
+# Initialise the start_packet_pos to 0
+start_packet_pos = 0
+
+# Initialise the loop counter to 0
+i = 0
+# Loop through each character of lines
+while i < len(lines):
+    # Convert 14 characters at a time into a set and then check their length. If
+    # there are repeating characters, then the length will reduce and no longer
+    # be equal to the original length (14). When the first occurence of 4 unique
+    # characters in a row is found, the if statement will be true
+    if len(set(lines[i:i+14])) == len(lines[i:i+14]):
+        # Update the start packet position to be 14 positions after i. Python
+        # indexes at 0, but the packet position is indexed at 1, so need to +14
+        # instead of +13
+        start_packet_pos = i + 14
+        # Break out of the loop
+        break
+    else:
+        # Increment the loop counter if starter packet not found
+        i += 1
+
+# Print the start packet position
+print(start_packet_pos)
